fa6a79e429f16e371077751856b1d1f18534f5de
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-06-12 08:17:50 +0700

    setup router

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
+import routes from "../server/src/routes/index";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use("/api/v1", routes);
 
 const port = process.env.PORT || 5000;
 
